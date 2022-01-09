8fd8b062d3a9c6b75cf85419c2b3496afd8385bf
Annie Zhou <a.zhou4847@gmail.com>
2022-01-09 12:46:35 -0500

    Finished basic layout of index page (will add more later)

---
+++ index.js
@@ -0,0 +1,10 @@
+/* This will handle the webpage responsiveness and animations on the webpage*/
+/* This will also handle dark mode configuration */
+
+function excMyWorkDD()
+{
+    //Show links if user clicks My Work Drop Down
+    document.getElementById("my_work_dd").classList.toggle("show");
+}
+
+//Closing My Work Drop Down if user 
