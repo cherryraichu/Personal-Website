fe6c27941436c1ea5ccb7196e8ef66e940b6e73f
Annie Zhou <a.zhou4847@gmail.com>
2022-01-09 21:27:26 -0500

    Added a button (missing most functionality) and fixed some bugs

--- js/global.js
+++ js/global.js
@@ -1,10 +1,19 @@
 /* This will handle the webpage responsiveness and animations on the webpage*/
 /* This will also handle dark mode configuration */
 
+
+//Show links if user clicks "My Work" drop down button
 function excMyWorkDD()
 {
-    //Show links if user clicks My Work Drop Down
-    document.getElementById("my_work_dd").classList.toggle("show");
+    var showMyWork = document.getElementById("mywork_dd");
+
+    console.log(showMyWork);
+    if (showMyWork.style.display == "block") {
+        showMyWork.style.display = "none";
+    } else {
+        showMyWork.style.display = "block";
+
+    }
 }
 
-//Closing My Work Drop Down if user 
+//Closing "My Work" drop down if user clicks elsewhere on page  
