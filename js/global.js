c2b3a7a62c793eacf28f66502a648bd03d2b3ecd
Annie Zhou <a.zhou4847@gmail.com>
2022-01-13 17:48:01 -0500

    Finished some dropdown js functionality

--- js/global.js
+++ js/global.js
@@ -1,19 +1,14 @@
-/* This will handle the webpage responsiveness and animations on the webpage*/
-/* This will also handle dark mode configuration */
-
 
 //Show links if user clicks "My Work" drop down button
-function excMyWorkDD()
+function showMyWorkDD(event)
 {
-    var showMyWork = document.getElementById("mywork_dd");
-
-    console.log(showMyWork);
-    if (showMyWork.style.display == "block") {
-        showMyWork.style.display = "none";
-    } else {
-        showMyWork.style.display = "block";
-
-    }
+    event.stopPropagation();
+    document.getElementById("mywork_dd").classList.toggle("show");
 }
 
-//Closing "My Work" drop down if user clicks elsewhere on page  
+//Closing "My Work" drop down if user clicks elsewhere on page 
+window.onclick = function(event)
+{
+    showMyWorkDD(event);
+} 
+
