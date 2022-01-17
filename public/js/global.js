4e72b85b306117bd6b0fe21f223704c61219d053
Annie Zhou <a.zhou4847@gmail.com>
2022-01-17 13:49:18 -0500

    Fixed some Js issue with dropdown bar

--- public/js/global.js
+++ public/js/global.js
@@ -1,8 +1,11 @@
 var navbarClick = document.querySelector(".topnav");
+var ddVisibility = null; 
 
 //Show links if user clicks "My Work" drop down button
 function showMyWorkDD(event)
 {
+    ddVisibility = window.getComputedStyle(document.getElementById("mywork_dd"));
+
     event.stopPropagation();
     document.getElementById("mywork_dd").classList.toggle("show");
 }
@@ -10,15 +13,21 @@ function showMyWorkDD(event)
 //Closing "My Work" drop down if user clicks elsewhere on page 
 document.addEventListener("click", function(event)
 {
-    if (event.target.closest(".topnav") ) 
+    if (event.target.closest(".topnav") || ddVisibility == null
+    || ddVisibility.display == "none") 
     {
+        //Don't toggle dropdown if user clicked on the navbar
+        //Don't toggle dropdown if the dropdown is already hidden
         return;
-    } 
-    else
+    }
+    else 
     {
+        //Toggle dropdown
         event.stopPropagation();
         showMyWorkDD(event);
     }
 });
+
+
 
 
